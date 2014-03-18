529d0b5c44038458de4b862deeb920d3d3c2da3f
Morten Winther Olsson <tweelie@alan-t>
2014-03-18 21:32:57 +0100

    Debug

--- A3/src/Neuron.py
+++ A3/src/Neuron.py
@@ -1,13 +1,14 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import math
 import numpy as np
 
-def __mult(x, y):
+def mult((x, y)):
     return x * y
 
-def __weighted_sum(vec_ins):
-    lambda vec_weights: sum(map(__mult, filter(lambda (w, i): w > 0, zip(vec_weights, vec_ins))))
+def weighted_sum(vec_ins):
+    return lambda vec_weights: sum(map(mult, filter(lambda (w, i): w > 0, zip(vec_weights, vec_ins))))
 
 class NeuronLayer:
     """NeuronLayer."""
@@ -15,7 +16,7 @@ class NeuronLayer:
         self.in_weights = mat_in_weights
         self.activs = [0]*len(mat_in_weights)
         self.outs = [0]*len(mat_in_weights)
-        self.ins = [0]*len(np.matrix(mat_in_weights).T.A2)
+        self.ins = [0]*len(np.matrix(mat_in_weights).T.A.tolist())
         self.deltas = [0]*len(mat_in_weights)
         self.error_deriv_w = np.matrix([[0]*len(self.ins)]*len(self.outs))
         self.activ = fun_activ
@@ -28,28 +29,43 @@ class NeuronLayer:
         self.error_deriv_w = np.matrix([[0]*len(self.ins)]*len(self.outs))
     def forward(self, vec_ins):
         self.ins = [1]+vec_ins
-        self.activs = map(__weighted_sum(vec_ins), self.in_weights)
+        self.activs = map(weighted_sum(self.ins), self.in_weights)
         self.outs = map(self.activ, self.activs)
         if (self.next != None):
             return self.next.forward(self.outs)
         return self.outs
     def backprop_error(self, vec_targets):
+        if(self.next != None):
+            self.next.backprop_error(vec_targets)
         self.deltas = map(lambda (y, t): y-t, zip(self.outs, vec_targets))
+        err_dw = np.matrix(map(lambda (delta, zs): map(lambda z: delta*z, zs), zip(self.deltas, [self.ins]*len(self.deltas))))
+        self.error_deriv_w += err_dw
+        if(self.prev != None):
+            self.prev.backprop_error_hidden()
+    def backprop_error_hidden(self):
+        self.deltas = map(mult, zip(map(self.act_deriv, self.outs), map(weighted_sum(self.next.deltas), np.matrix(self.next.in_weights).T.A.tolist())))
         self.error_deriv_w += np.matrix(map(lambda (delta, zs): map(lambda z: delta*z, zs), zip(self.deltas, [self.ins]*len(self.deltas))))
         if(self.prev != None):
-            self.prev.backprop_error()
-    def backprop_error(self):
-        self.deltas = map(__mult, zip(map(self.act_deriv, self.outs), map(__weighted_sum(next.deltas), np.matrix(next.in_weights).T.A2)))
-        self.error_deriv_w += np.matrix(map(lambda (delta, zs): map(lambda z: delta*z, zs), zip(self.deltas, [self.ins]*len(self.deltas))))
-        if(self.prev != None):
-            self.prev.backprop_error()
+            self.prev.backprop_error_hidden()
 
 
 def init_network(weights, activation_funs, actfun_derivations):
-    return reduce(lambda nl_next, (mat_in_weights, (fun_activ, fun_act_deriv)): NeuronLayer(mat_in_weights, fun_activ, fun_act_deriv, nl_next), zip(weights, zip(activation_funs, actfun_derivations))[:0:-1], NeuronLayer(weights[-1]))
+    return reduce(lambda nl_next, (mat_in_weights, (fun_activ, fun_act_deriv)): NeuronLayer(mat_in_weights, fun_activ, fun_act_deriv, nl_next), zip(weights, zip(activation_funs, actfun_derivations))[-2::-1], NeuronLayer(weights[-1]))
 
 def train_pattern(network, xs, ts):
     network.forward(xs)
     network.backprop_error(ts)
+
+hidden_ws = [[1, 1], [1, 1], [1, 1]]
+hidden_activ = lambda a: a/(1+abs(a))
+hidden_activ_deriv = lambda a: 1/math.pow(1+abs(a), 2)
+output_ws = [[1, 1, 1, 1]]
+output_activ = id
+output_activ_deriv = None
+network = init_network([hidden_ws, output_ws], [hidden_activ, output_activ], [hidden_activ_deriv, output_activ_deriv])
+samples = map(lambda x: (x, (math.sin(x)/x)+np.random.normal(0, .004, 1)[0]), (np.random.rand(1000)*32).tolist())
+map(lambda (x, t): train_pattern(network, [x], [t]), samples)
+print(str(network.error_deriv_w))
+print(str(network.next.error_deriv_w))
 
 # def train_testdata(network
