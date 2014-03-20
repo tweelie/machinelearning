c73934c85c56e1b3481e8be630761a26b034c964
Morten Winther Olsson <tweelie@alan-t>
2014-03-20 02:30:32 +0100

    Working Neural Network!

--- A3/src/Neuron.py
+++ A3/src/Neuron.py
@@ -29,11 +29,16 @@ class NeuronLayer:
     def gradient(self):
         if (self.next == None):
             return [self.error_deriv_w/self.samples]
-        return self.next.gradient()+[self.error_deriv_w/self.samples]
+        return [self.error_deriv_w/self.samples]+self.next.gradient()
     def apply_and_reset(self, eta):
         self.in_weights = (np.matrix(self.in_weights)-(eta*self.error_deriv_w/self.samples)).A.tolist()
         self.error_deriv_w = np.matrix([[.0]*len(self.ins)]*len(self.outs))
         self.samples = 0
+    def set_weights(self, weights):
+        self.in_weights = weights[0]
+        if (self.next != None):
+            # print("ws: "+str(self.in_weights))
+            self.next.set_weights(weights[1:])
     def forward(self, vec_ins):
         self.ins = [1]+vec_ins
         # print("Ins: "+str(self.ins))
@@ -46,18 +51,27 @@ class NeuronLayer:
         return self.outs
     def backprop_error(self, vec_targets):
         if(self.next != None):
-            self.next.backprop_error(vec_targets)
-        self.deltas = map(lambda (y, t): y-t, zip(self.outs, vec_targets))
-        err_dw = np.matrix(map(lambda (delta, zs): map(lambda z: delta*z, zs), zip(self.deltas, [self.ins]*len(self.deltas))))
-        #print(str(err_dw))
+            return self.next.backprop_error(vec_targets)
+        # print("t: "+str(vec_targets))
+        # print("outs(y): "+str(self.outs))
+        # print("ins(z): "+str(self.ins))
+        # print("(y[0]-t[0])*zs: "+str((self.outs[0]-vec_targets[0])*np.array(self.ins)))
+        self.deltas = map(lambda y, t: y-t, self.outs, vec_targets)
+        err_dw = np.matrix(map(lambda delta, zs: map(lambda z: delta*z, zs), self.deltas, [self.ins]*len(self.deltas)))
+        # print("err_dw: "+str(err_dw))
         self.error_deriv_w += err_dw
         self.samples += 1
         if(self.prev != None):
             self.prev.backprop_error_hidden()
     def backprop_error_hidden(self):
-        self.deltas = map(mult, zip(map(self.act_deriv, self.outs), map(weighted_sum(self.next.deltas), np.matrix(self.next.in_weights).T.A.tolist())))
-        err_dw = np.matrix(map(lambda (delta, zs): map(lambda z: delta*z, zs), zip(self.deltas, [self.ins]*len(self.deltas))))
-        #print(str(err_dw))
+        # print("xs: "+str(self.ins))
+        # print("as: "+str(self.activs))
+        # print("ws: "+str(self.in_weights))
+        # print("next.deltas: "+str(self.next.deltas))
+        self.deltas = map(mult, zip(map(self.act_deriv, self.activs), map(weighted_sum(self.next.deltas), np.matrix(self.in_weights).A.tolist())))
+        # print("deltas: "+str(self.deltas))
+        err_dw = np.matrix(map(lambda (delta, xs): map(lambda x: delta*x, xs), zip(self.deltas, [self.ins]*len(self.deltas))))
+        # print("err_dw: "+str(err_dw))
         self.error_deriv_w += err_dw
         self.samples += 1
         if(self.prev != None):
@@ -67,9 +81,17 @@ class NeuronLayer:
 def init_network(weights, activation_funs, actfun_derivations):
     return reduce(lambda nl_next, (mat_in_weights, (fun_activ, fun_act_deriv)): NeuronLayer(mat_in_weights, fun_activ, fun_act_deriv, nl_next), zip(weights, zip(activation_funs, actfun_derivations))[-2::-1], NeuronLayer(weights[-1]))
 
+def s_error(ys, ts):
+    err = np.matrix(ys)-np.matrix(ts)
+    return (err.T*err).A1[0]/2.0
+
+def ms_error(ysli, tsli):
+    return sum(map(s_error, ysli, tsli))/len(ysli)
+
 def train_pattern(network, xs, ts):
-    network.forward(xs)
+    ys = network.forward(xs)
     network.backprop_error(ts)
+    return ys
 
 hidden_ws = [[1, 1], [1, 1], [1, 1]]
 hidden_activ = lambda a: a/(1+abs(a))
@@ -79,18 +101,58 @@ output_activ = id
 output_activ_deriv = None
 network = init_network([hidden_ws, output_ws], [hidden_activ, output_activ], [hidden_activ_deriv, output_activ_deriv])
 samples = map(lambda x: (x, (math.sin(x)/x)+np.random.normal(0, .004, 1)[0]), (np.random.rand(1000)*32).tolist())
-grad_len = 100.0
-tmp = 32
-while(tmp > 1.0):
-    map(lambda (x, t): train_pattern(network, [x], [t]), samples)
-    grad = network.gradient()
-    print(str(grad))
-    print(str(network.in_weights))
-    print(str(network.next.in_weights))
-    grad_len = sum(map(lambda (acc, n): acc/n, map(lambda mat: reduce(lambda (acc, n), vec: (acc+math.sqrt(sum((np.array(vec)**2).tolist())), n+1), mat.A.tolist(), (.0, 0)), grad)))/len(grad)
-    print(str(grad_len))
-    network.apply_and_reset(.1)
-    tmp -= 1
+
+e = 2**-22
+hidden_e = [[0, 1],[2, 3],[4, 5]]
+output_e = [[6, 7, 8, 9]]
+def e_or_zero(w, n_e):
+    if(w == n_e):
+        return e
+    return .0
+def e_setter(ws_li, n_e):
+    return map(lambda ws: map(lambda w: e_or_zero(w, n_e), ws), ws_li)
+
+ysli0 = map(lambda (x, t): train_pattern(network, [x], [t]), samples)
+tsli = map(lambda (x, t): [t], samples)
+err0 = ms_error(ysli0, tsli)
+grad_bp = network.gradient()
+grad_num = [.0, .1, .2, .3, .4, .5, .6, .7, .8, .9]
+for i in range(10):
+    network.apply_and_reset(1.0)
+    network.set_weights([(np.matrix(hidden_ws)+np.matrix(e_setter(hidden_e, i))).A.tolist(),
+                        (np.matrix(output_ws)+np.matrix(e_setter(output_e, i))).A.tolist()])
+    yslii = map(lambda (x, t): train_pattern(network, [x], [t]), samples)
+    erri = ms_error(yslii, tsli)
+    grad_num[i] = (erri - err0)/e
+
+print("Back-propagated gradient:\n")
+print(str(grad_bp))
+print("Numerically estimated gradient (e: "+str(e)+"):\n")
+print(str(map(lambda mat: np.matrix(map(lambda vec: map(lambda i: grad_num[i], vec), mat)), [hidden_e, output_e])))
+
+# grad_len = 100.0
+# tmp = 32
+samps = 1000
+errpow = 4.0
+smppow = 2.0
+eta = .1
+ms_err = (2.0**1023)**(1/errpow)
+d_ms_err = 1.0 # 2.0**1023
+while(d_ms_err*(ms_err**errpow)/(samps**(1.0/smppow)) > .6):
+    network.apply_and_reset(eta)
+    ysli = map(lambda (x, t): train_pattern(network, [x], [t]), samples)
+    # grad = network.gradient()
+    new_ms_err = ms_error(ysli, tsli)
+    d_ms_err = ms_err-new_ms_err
+    ms_err = new_ms_err
+    # print("\nwhile: "+str(d_ms_err*(ms_err**errpow)/(samps**(1.0/smppow))))
+    # print("\ngrad: "+str(grad))
+    # print("ws[hidden]: "+str(network.in_weights))
+    # print("ws[out]: "+str(network.next.in_weights))
+    # print("ms_err: "+str(ms_err))
+    # grad_len = sum(map(lambda (acc, n): acc/n, map(lambda mat: reduce(lambda (acc, n), vec: (acc+math.sqrt(sum((np.array(vec)**2).tolist())), n+1), mat.A.tolist(), (.0, 0)), grad)))/len(grad)
+    # print(str(grad_len))
+    # tmp -= 1
 
 # map(lambda (x, t): train_pattern(network, [x], [t]), samples)
 # print(str(network.gradient()))
